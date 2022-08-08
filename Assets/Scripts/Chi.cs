1186cc9a881d0ffb249099a7b2bcd3208b8f41e4
John Butler <jbutler5123@gmail.com>
2022-08-08 11:49:06 -0400

    Prefabs created, added functionality for child vocalization(), added random spawning of prefabs.

--- Assets/Scripts/Chi.cs
+++ Assets/Scripts/Chi.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Chi : Dog
 {
     //INHERITANCE - ANIMAL -> DOG
+
+    public GameObject chiVocalizeCanvas;
+    public GameObject chiVocalizeText;
     //POLYMORPHISM - OVERRIDES DOG AND ANIMAL
     public override void Vocalize()
     {
         //Yap
         Debug.Log("Yap");
+        chiVocalizeCanvas.SetActive(true);
+        chiVocalizeText.GetComponent<TMPro.TextMeshProUGUI>().text="yap";
     }
 
     //POLYMORPHISM - OVERRIDES ANIMAL
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject catPrefab;
+    public GameObject dogPrefab;
+    public GameObject chiPrefab;
+    public GameObject huskyPrefab;
+    //private GameObject[] animals = new GameObject[4];
+    private float spawnRange=8.0f;
+    //private GameObject[] animalsPrefabs;
+    static public List<GameObject> animals = new List<GameObject>();
+
+    //private List<String> animals = new List<String>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        animals.Add(catPrefab);
+        animals.Add(dogPrefab);
+        animals.Add(chiPrefab);
+        animals.Add(huskyPrefab);
+        SpawnAnimals();
+    }
+
+    private void SpawnAnimals()
+    {
+        foreach (var animal in animals)
+        {
+            for (int i = 0; i < UnityEngine.Random.Range(2,5); i++)
+            {
+                Instantiate(animal,GenerateSpawnPosition(),animal.transform.rotation);
+            }
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX= UnityEngine.Random.Range(-spawnRange,spawnRange);
+        float spawnPosZ=UnityEngine.Random.Range(-spawnRange,spawnRange);
+        Vector3 spawnPos= new Vector3(spawnPosX,5,spawnPosZ);
+
+        return spawnPos;
+    }
+}
